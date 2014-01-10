d84cffa4b3503b15fbc96e16337dc8c61272838f
Nikos M <nikosms0@gmail.com>
2014-01-10 13:54:05 +0200

    update buildtools

--- src/MOD3.js
+++ src/MOD3.js
@@ -1,13 +1,11 @@
 /**
 *
-* MOD3 3D Modifier Library for JavaScript
-* port of AS3DMod ActionScript3 library (http://code.google.com/p/as3dmod/)
+*   MOD3.js
+*   @version: @@VERSION@@
 *
-* http://github.com/foo123/MOD3
-*
-* @supports Three.js, J3D, Copperlicht, CubicVR.js, Pre3D
-*
-* @author Nikos M. http://nikos-web-development.netai.net/
+*   MOD3 3D Modifier Library for JavaScript 
+*   port of AS3DMod ActionScript3 library (http://code.google.com/p/as3dmod/)
+*   https://github.com/foo123/MOD3
 *
 **/
 var Class = Classy.Class;
